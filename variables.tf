2ea23e7abce0ea645ab98e4dc64dbb61167e82ac
Nyaine Chan Myint <45549387+NyaineChanMyint@users.noreply.github.com>
2021-11-15 23:54:49 +0630

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,7 @@
-#variable "ami" {
-#type = string
-#}
 
 variable "instance_type" {
 type = string
 }
-
-#variable "key_name" {
-#type = string
-#}
-
-#variable "monitoring" {
-#type = bool
-#}
-
-#variable "vpc_security_group_ids" {
-#type = list(string)
-#}
-
-#variable "subnet_id" {
-#type = string
-#}
-
 variable "instance_name" {
 type = string
 }
